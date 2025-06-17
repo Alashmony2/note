714da12cf005448a27b0a286b4385337f553fb35
ahmed <ahmedSamyAlashmony1@gmail.com>
2025-06-17 19:55:05 +0300

    adding route , Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-
-import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Layout from './components/Layout/Layout';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import NotFound from './pages/NotFound/NotFound';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",element:<Layout/>,children:[
+        {index:true,element:<Home/>},
+        {path:"login",element:<Login/>},
+        {path:"register",element:<Register/>},
+        {path:"*",element:<NotFound/>},
+      ]
+    }
+  ])
+
   return (
     <>
+      <RouterProvider router={router}/>
     </>
   )
 }
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
 
 export default function Layout() {
-  return (
-    <div>Layout</div>
-  )
+  return <>
+    
+    <Navbar/>
+  <div className="p-4 sm:ml-64">
+    <Outlet/>
+  </div>
+
+
+
+
+  </>
+  
 }
